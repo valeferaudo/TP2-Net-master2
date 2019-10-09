563d9bb40a6857a0791b282772ce25264007def7
valeferaudo <valentinferaudo@gmail.com>
2019-10-09 20:39:54 -0300

    ABM web Planes

---
+++ UI.Web/Planes.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="Planes" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Planes.aspx.cs" Inherits="UI.Web.Planes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+    <asp:Panel ID="gridPanel" runat="server">
+        <asp:GridView ID="gridViewPlan" runat="server" AutoGenerateColumns="false" SelectedRowStyle-BackColor="Black" 
+            SelectedRowStyle-ForeColor="White" DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField HeaderText="ID" DataField="ID" />
+                <asp:BoundField HeaderText="Descripcion" DataField="Descripcion" />
+                <asp:BoundField HeaderText="Especialidad" DataField="IDEspecialidad" />
+                <asp:CommandField SelectText="Seleccionar" ShowSelectButton="true" />
+            </Columns>
+        </asp:GridView>
+        <asp:Panel ID="gridActionsPanel" runat="server">
+            <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+            <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
+            <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click">Nuevo</asp:LinkButton>
+        </asp:Panel>
+    </asp:Panel>
+    <asp:Panel ID="formPanel" Visible="false" runat="server">
+        <asp:Label ID="descripcionLabel" runat="server" Text="Descripcion"></asp:Label>
+        <asp:TextBox ID="descripcionTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="idEspecialidadLabel" runat="server" Text="Especialidad"></asp:Label>
+        <asp:TextBox ID="idEspecialidadTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Panel ID="formActionsPanel" runat="server">
+            <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click">Aceptar</asp:LinkButton>
+            <asp:LinkButton ID="cancelarLinkButton" runat="server" OnClick="cancelarLinkButton_Click">Cancelar</asp:LinkButton>
+        </asp:Panel>
+    </asp:Panel>
+</asp:Content>
---
+++ UI.Web/Planes.aspx.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Logic;
+using Business.Entities;
+
+namespace UI.Web
+{
+    public partial class Planes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.Page.IsPostBack)
+            {
+                this.LoadGrid();
+            }
+            else
+            {
+                this.LoadGrid();
+            }
+
+        }
+        PlanLogic _plan;
+        private PlanLogic Plan
+        {
+            get
+            {
+                if (_plan == null)
+                {
+                    _plan = new PlanLogic();
+                }
+                return _plan;
+            }
+        }
+        private void LoadGrid()
+        {
+            this.gridViewPlan.DataSource = this.Plan.GetAll();
+            this.gridViewPlan.DataBind();
+        }
+        public enum formModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+
+        public formModes formMode
+        {
+            get { return (formModes)this.ViewState["formMode"]; }
+            set { this.ViewState["formMode"] = value; }
+        }
+        private Plan Entity
+        {
+            get;
+            set;
+        }
+
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool IsEntitySelected
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridViewPlan.SelectedValue;
+        }
+        private void LoadForm(int ID)
+        {
+            this.Entity = this.Plan.GetOne(ID);
+            this.descripcionTextBox.Text = this.Entity.Descripcion;
+            this.idEspecialidadTextBox.Text = (this.Entity.IDEspecialidad).ToString();
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.formMode = formModes.Modificacion;
+                this.EnableForm(true);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+        private void LoadEntity(Plan plan)
+        {
+            plan.Descripcion = this.descripcionTextBox.Text;
+            plan.IDEspecialidad = Int32.Parse(this.idEspecialidadTextBox.Text);
+
+        }
+        private void SaveEntity(Plan plan)
+        {
+            this.Plan.Save(plan);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.formMode)
+            {
+                case formModes.Alta:
+                    {
+                        this.Entity = new Plan();
+                        this.Entity.State = BusinessEntity.States.New;
+                        this.LoadEntity(this.Entity);
+                        this.SaveEntity(this.Entity);
+                        this.LoadGrid();
+
+
+                    }
+                    break;
+                case formModes.Baja:
+                    {
+                        //this.Entity.State = BusinessEntity.States.Deleted;
+                        //TIRA ERROR
+                        this.DeleteEntity(this.SelectedID);
+                        this.LoadGrid();
+                    }
+                    break;
+                case formModes.Modificacion:
+                    {
+                        this.Entity = new Plan();
+                        this.Entity.ID = this.SelectedID;
+                        this.Entity.State = BusinessEntity.States.Modified;
+                        this.LoadEntity(this.Entity);
+                        this.SaveEntity(this.Entity);
+                        this.LoadGrid();
+                    }
+                    break;
+                default:
+                    break;
+            }
+            this.LoadGrid();
+
+            this.formPanel.Visible = false;
+        }
+        private void EnableForm(bool enable)
+        {
+            this.descripcionTextBox.Enabled = enable;
+            this.descripcionLabel.Visible = enable;
+            this.idEspecialidadTextBox.Enabled = enable;
+            this.idEspecialidadLabel.Visible = enable;
+
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.formMode = formModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+        private void DeleteEntity(int ID)
+        {
+            this.Plan.Delete(ID);
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.formMode = formModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+        private void ClearForm()
+        {
+            this.descripcionTextBox.Text = string.Empty;
+            this.idEspecialidadTextBox.Text = string.Empty;
+
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = false;
+            this.ClearForm();
+        }
+    }
+
+}
