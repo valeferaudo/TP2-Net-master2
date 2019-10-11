811cd697f5711306049cae96a733746983188838
Juan Fogliato <juan.agustin.fogliato@gmail.com>
2019-10-10 21:20:38 -0300

    clase validacion tipo datos en util

---
+++ Util/ValidacionIngresoDatos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public class ValidacionIngresoDatos
+    {
+        public bool EsNumero(String strnro)
+        {
+            int nro = 0;
+            bool result;
+            try
+            {
+                nro = Int32.Parse(strnro);
+                result = true;
+            }
+            catch (FormatException)
+            {
+                result = false;
+            }
+            return result;
+        }
+        public bool EsMail(string email)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(email);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+    
+}
