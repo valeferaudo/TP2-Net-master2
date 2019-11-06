46fdf5273c245e633c7d81b921ce81cf425c289a
Juan Fogliato <juan.agustin.fogliato@gmail.com>
2019-11-06 14:09:10 -0300

    Logout

---
+++ UI.Web/Logout.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UI.Web
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["UsuarioSesion"] = null;
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+}
